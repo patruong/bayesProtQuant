8d01960f4d1c69317d663445aff1c67e40da1376
patruong <ptruong@kth.se>
2019-06-18 15:38:25 +0200

    new brnch

--- src/clusterRun.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.chdir("triqler_")
-cmd = "python triqler.py -h" 
-#cmd = "touch __init__.py"
-os.system(cmd)
--- src/getUniqueProteins.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat 500-PSSS3-equ\ decoy_Report.xls | cut -f 4 | sort | uniq -c > proteins.csv
-
-
