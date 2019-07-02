4cc587df0da06b6f71ffdb00e71ee84343742e68
patruong <ptruong@kth.se>
2019-07-02 17:15:11 +0200

    added more experiment scripts

---
+++ results/2019-07-03_volcanoPlot2_diffExp/plotting.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 10:41:19 2019
+
+@author: ptruong
+"""
+import sys 
+
+from proteinSearch import *
+
+from triqlerParser import *
+from triqlerProcessor import * 
+from utils import * 
+from parseReformattedPSSS3equDecoy import * # Needs to tidy and place functions in right place!
+from proc import *
+#from corrPlot import * #correlation plotting
+
+import os
+import argparse
+import pandas as pd
+import numpy as np
+from scipy import stats
+import scipy as sp
+import subprocess
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+def comparisonPlot(sampgroup, specie, hardRange = True, truncated = False, title = "plot"):
+    
+    homoSapiens = ["hs", "HS", "homo sapiens", "Homo Sapiens", "Homo sapeins", 
+                   "homosapiens", "HomoSapiens", "HUMAN", "human",
+                   "H sapeins", "h sapiens", "H Sapeins"]
+    caenorhabditisElegans = ["ce", "CE", "Caenorhabditis elegans", "Caenorhabditis Elegans",
+                             "caenorhabditis elegans", "caenorhabditiselegans", "CaenorhabditisElegans",
+                             "CAEEL", "caeel",
+                             "C elegans", "c elegans", "C Elegans"]
+    arabidopsisThaliana = ["at", "AT", "Arabidopsis thaliana", "Arabidopsis Thaliana",
+                           "arabidopsis thaliana", "arabidopsisthaliana", "ArabidopsisThaliana",
+                           "ARATH", "arath",
+                           "A thaliana", "a thaliana", "A Thaliana"]
+    
+    species = homoSapiens + caenorhabditisElegans + arabidopsisThaliana
+    
+    if specie not in species:
+        raise Exception("Incorrect specie!")
+    elif specie in homoSapiens:
+        a_mix, c_mix, h_mix, mix = getMixtures(truncated = truncated)
+        specie = "H sapiens"
+        species_mixture = h_mix
+    elif specie in caenorhabditisElegans:
+        a_mix, c_mix, h_mix, mix = getMixtures(truncated = truncated)
+        specie = "C elegans"
+        species_mixture = c_mix
+    elif specie in arabidopsisThaliana:
+        a_mix, c_mix, h_mix, mix = getMixtures(truncated = truncated)
+        specie = "A thaliana"
+        species_mixture = a_mix
+    
+
+    fig, ax = plt.subplots(figsize=(13, 8))
+    #sns.violinplot(data=sampgroup.loc[sampgroup['Species'] == "H sapiens"], x="Sample",y="Expression", hue="Method",split=True)
+    sns.boxplot(data=sampgroup.loc[sampgroup['Species'] == specie], x="Sample",y="Expression", hue="Method",fliersize=0)
+    for i in range(len(species_mixture)):
+        ax.plot([i-.2, i+.2], [species_mixture[i], species_mixture[i]], '-', color='red', lw=5)
+    #ax.set_ylim(0,0.26)
+    
+    if hardRange == False:
+        nonOutlierMax = sampgroup.loc[sampgroup['Species'] == specie]["Expression"].quantile([0.99]).values[0]
+        ax.set_ylim(0, nonOutlierMax)
+    else:
+        if specie == "H sapiens":
+            ax.set_ylim(0,0.26)
+        elif specie == "C elegans":
+            ax.set_ylim(0,0.9)
+        elif specie == "A thaliana":
+            ax.set_ylim(0,0.2)
+    ax.set_ylabel("Fraction of protein total abundance per sample")
+    sns.despine(offset=10, trim=True)
+    plt.title(specie + " " + title)  
+    fig.savefig(specie+ "_" + title+".png")
+
+#############################
+## CORRELATION COEFFICIENTS #
+#############################
+# CORRELATION PLOTTING FUNCTIONS.
+from scipy.stats.stats import pearsonr
+from numpy import log10
+
+def my_corr(row):
+    repl_cols = ["S01","S02","S03","S04","S05","S06","S07","S08","S09","S10"]
+    a_mix, c_mix, h_mix, mix = getMixtures()
+    mymix = mix[row['Species']]
+    row = row.loc[repl_cols]
+    ixs = (row != 0.0)
+    a = np.array(row.loc[ixs])
+    b = mymix[ixs]
+    return pearsonr(a,b)[0]
+
+def p_r_plot(unmeltsamp, species,figname=None):
+    unmeltsamp['Correlation, R'] = unmeltsamp.apply(my_corr, axis=1)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    N=5000
+    for meth in unmeltsamp['Method'].unique():
+        sns.distplot(unmeltsamp[(unmeltsamp['Method']==meth) & (unmeltsamp['Species']==species)]['Correlation, R'],
+             hist_kws={"cumulative":-1, "linewidth": 3, "alpha": 1,"histtype": "step"},
+             bins=N, kde=False, ax=ax, label=meth) #, norm_hist = )
+    ax.set_xlim(-0.8,1.0)
+    #ax.set_ylim(0,1.2)
+    ax.set_title(species)
+    sns.despine(offset=10, trim=True)
+    ax.set_ylabel("Number of proteins")
+    ax.set_xlabel("Cumulative samples "r"$\leq$" " correlation coefficient")
+    plt.legend()
+    #if figname:
+    fig.savefig(figname)
+
+#######################################
+# GENERATE DIFF EXPRESSION COMPARISON #
+#######################################
+
+def generateDiffExpPlot(fileDirectory, df_psss3, FDR_treshold, logSpectronaut, figName = "plot"):
+        comparisons = getComparisons(fileDir = fileDirectory, filePrefix = "proteins", fileSuffix = ".tsv")
+        comparisons.remove([''])
+        triqlerComps  = getTriqlerComparisons(comparisons, prefix = "proteins", suffix = "tsv")
+        spectronautComps = getSpectronautComparison(comparisons)
+        
+        # Input directory of triqler outputs    
+        diffExpDir = fileDirectory
+        FDRTreshold = FDR_treshold
+        specieList = [None, "ARATH", "CAEEL", "HUMAN"]
+        for k in specieList:
+            specie = k
+            if specie == None:
+                print("Generating AllSpecies DiffExp...")
+            else:
+                print("Generating " + specie + " DiffExp...")
+            diffExpComps = processComparisons(triqlerOutputDir = diffExpDir,
+                               PSSS3 = df_psss3,
+                               comparisons = comparisons,
+                               triqlerComps = triqlerComps,
+                               spectronautComps = spectronautComps,
+                               FDRTreshold = FDRTreshold,
+                               specie = specie,
+                               logSpectronaut = logSpectronaut)
+            comp_df = neatifyDiffExpComps(diffExpComps, comparisons)
+            all_df = meltCompDf(comp_df)
+            if specie == None:
+                specieTitle = "AllSpec"
+            else:
+                specieTitle = specie
+            diffExpTitle = specieTitle + figName
+            
+            barplotComps(all_df, diffExpTitle, 
+                         fileDirectory + "/results/" + specieTitle+'_diffExpPlot'+figName+".png")
+
+from math import sin
+from random import random
+
+from bokeh.io import output_file, show
+
+import bokeh.plotting as bpl
+import bokeh.models as bmo
+
+def nanAdjustCol(df, booleandf):
+    nandf = (np.isnan(df["log2fc"]) | np.isnan(df["logFDR"]))
+    nanDict = {True: np.nan, False: True}
+    nandf = nandf.map(nanDict)
+    booleandf = nandf*booleandf
+    return booleandf
+
+def volcano_df_format(log2fc, log2P, logFunc = np.log2, side = "two", fc_treshold = 1.0, fc_dev = 0.2, p_treshold = 0.05, col_diffExp = "red", col_not_diffExp = "blue"):
+    """
+    log2fc - as pandas series with protein names indices.
+    log2P - as pandas series with protein names indices.
+    fc_dev - if side = "fc", how wide should the interval around the fc be.
+    """
+    # params
+
+    side = side #Two or one-sided treshold ("two", "left" or "right")
+    fc_treshold = fc_treshold
+    p_treshold = p_treshold
+    logP_treshold = -logFunc(p_treshold)
+    
+    # Checking data
+    list_x = log2fc.tolist()
+    index_x = log2fc.index
+    list_y = log2P.tolist()
+    index_y = log2P.index
+    if any(index_x != index_y):
+        raise Exception("index mismatch between x and y")
+    df = pd.DataFrame(
+            {
+                    "protein": index_x,
+                    "log2fc": list_x,
+                    "logFDR": list_y,
+                    
+            }
+    )
+    if ((side == "r") or (side == "right")):
+        print("Right sided fc treshold.")
+        booleandf = (df["log2fc"] > fc_treshold) & (df["logFDR"] > logP_treshold)
+        booleandf = nanAdjustCol(df, booleandf)
+    elif ((side == "l") or (side == "left")):
+        print("Left sided fc treshold.")
+        booleandf = (df["log2fc"] < -fc_treshold) & (df["logFDR"] > logP_treshold)
+        booleandf = nanAdjustCol(df, booleandf)
+    elif ((side == "two") or (side == "both")):
+        print("Two sided fc treshold.")
+        booleandf_right = ((df["log2fc"] > fc_treshold) & (df["log2P"] > logP_treshold))
+        booleandf_left = (df["log2fc"] < -fc_treshold) & (df["log2P"] > logP_treshold)
+        booleandf = pd.DataFrame(np.array([booleandf_right, booleandf_left]).T, columns = ["right", "left"])
+        booleandf = booleandf.any(axis = 1)
+        booleandf = nanAdjustCol(df, booleandf)
+    elif ((side == "fc")):
+        print("Interval around FC")
+        booleandf = ((df["log2fc"] > fc_treshold - fc_dev) & (df["logFDR"] > logP_treshold) & (df["log2fc"] < fc_treshold + fc_dev))
+        booleandf = nanAdjustCol(df, booleandf) 
+    booleanDictionary = {1: col_diffExp, 0: col_not_diffExp}
+    df["color"] = booleandf.map(booleanDictionary)
+    df["diffExp"] = booleandf.map({1: True, 0: False}) 
+    return df
+
+
+
+def volcanoPlot(df, vertical_line_r = None, vertical_line_l = None, horizontal_line = None,  plotWidth = 1000, plotHeight = 1000, x_label = "x-label", y_label = "y-label", title = "Volcano plot of Proteins", outputFile = None):
+    plot_width = plotWidth
+    plot_height = plotHeight
+    title = title
+    
+    
+    
+    source = bpl.ColumnDataSource.from_df(df)
+    hover = bmo.HoverTool(tooltips=[
+        ("(log2fc,logFDR)", "(@log2fc, @logFDR)"),
+        ('protein', '@protein'),
+    ])
+    boxSelect = bmo.BoxSelectTool()
+    p = bpl.figure(plot_width=plot_width, plot_height=plot_height, tools=[hover, boxSelect], 
+                   title=title,
+                   x_axis_label= x_label,
+                   y_axis_label= y_label)
+    
+    # Vertical line
+    if vertical_line_r != None:
+        vline_r = bmo.Span(location=vertical_line_r, dimension='height', line_color='red', line_dash = "dashed", line_width=1, line_alpha = 0.8)
+        p.renderers.extend([vline_r])
+    if vertical_line_l != None:
+        vline_l = bmo.Span(location=vertical_line_l, dimension='height', line_color='red', line_dash = "dashed", line_width=1, line_alpha = 0.8)
+        p.renderers.extend([vline_l])
+    # Horizontal line
+    if horizontal_line != None:
+        hline = bmo.Span(location=horizontal_line, dimension='width', line_color='red', line_dash = "dashed", line_width=1, line_alpha = 0.8)
+        p.renderers.extend([hline])
+    
+    p.scatter(
+        'log2fc', 
+        'logFDR', source=source, color='color')
+    if outputFile != None:
+        bpl.output_file(outputFile)
+    bpl.show(p)
+
+
+def volcanoPlot_spectronaut_triqler(df1, df2, vertical_line_r = None, vertical_line_l = None, horizontal_line = None, plot_width = 1000, plot_height = 1000, title = "plot", x_label = "xlabel", y_label = "ylabel", outputFile = None):
+    plot_width = plot_width
+    plot_height = plot_height
+    title = title
+    hover = bmo.HoverTool(tooltips=[
+        ("(log2fc,logFDR)", "(@log2fc, @logFDR)"),
+        ('protein', '@protein'),
+    ])    
+    boxSelect = bmo.BoxSelectTool()    
+    p = bpl.figure(plot_width=plot_width, plot_height=plot_height, tools=[hover, boxSelect], title=title, x_axis_label= x_label, y_axis_label= y_label)
+    
+    # Vertical line
+    if vertical_line_r != None:
+        vline_r = bmo.Span(location=vertical_line_r, dimension='height', line_color='red', line_dash = "dashed", line_width=1, line_alpha = 0.8)
+        p.renderers.extend([vline_r])
+    if vertical_line_l != None:
+        vline_l = bmo.Span(location=vertical_line_l, dimension='height', line_color='red', line_dash = "dashed", line_width=1, line_alpha = 0.8)
+        p.renderers.extend([vline_l])
+    # Horizontal line
+    if horizontal_line != None:
+        hline = bmo.Span(location=horizontal_line, dimension='width', line_color='red', line_dash = "dashed", line_width=1, line_alpha = 0.8)
+        p.renderers.extend([hline])
+    
+    for data, name in zip([df1, df2], ["Triqler", "Spectronaut"]):
+        df = pd.DataFrame(data)
+        
+        source = bpl.ColumnDataSource.from_df(df)
+
+        p.scatter('log2fc', 'logFDR', source=source, color='color', alpha=0.8,
+               muted_color='color', muted_alpha=0.05, legend=name)
+    p.legend.location = "top_left"
+    p.legend.click_policy="mute"
+    if outputFile != None:  
+        bpl.output_file(outputFile)    
+    bpl.show(p)
---
+++ src/experiment_volcano_diffExp.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 16:58:14 2019
+
+@author: ptruong
+"""
+
+
+from plotting import *
+from proc import *
+from func import *
+from log2FC import * 
+
+#############
+# Triqler  ##
+#############
+
+def get_triqler_volcano_df_pVals(triqlerFile, protein_id_fdr_treshold, specie, s1, s2, logFunc = np.log2, side = "two", fc_treshold = 1.0, fc_dev = 0.2, p_treshold = 0.05, col_diffExp = "red", col_not_diffExp = "blue", pickle_path = "../data/pickled/"):
+    """
+    s1,s2 = S01...S10
+    """
+    # Read data
+    triqler = read_pickle_Triqler(triqlerFile, protein_id_fdr_treshold, pickle_path = pickle_path)
+    at_t, ce_t, hs_t = splitTriqlerBySpecies(triqler, exponential = False, truncated = False)
+    
+    if specie == "HUMAN":
+        df = hs_t
+    elif specie == "ARATH":
+        df = at_t
+    elif specie == "CAEEL":
+        df = ce_t
+    #define samples
+    sample1 = df[s1]
+    sample2 = df[s2]
+    log2fc_vals = get_point_estimate_log2fc(sample1, sample2)
+    #log2P = compute_log2P(sample1, sample2)
+    #log10P = compute_log10P(sample1, sample2)
+    logP = compute_logP(sample1, sample2, logFunc)
+    #df_volcano_format = volcano_df_format(log2fc_vals, log2P[0], side = side, fc_treshold = fc_treshold, p_treshold = p_treshold, col_diffExp = col_diffExp, col_not_diffExp = col_not_diffExp)
+    df_volcano_format = volcano_df_format(log2fc_vals, logP[0], logFunc = logFunc, side = side, fc_treshold = fc_treshold, fc_dev = fc_dev, p_treshold = p_treshold, col_diffExp = col_diffExp, col_not_diffExp = col_not_diffExp)
+    return df_volcano_format
+
+def get_triqler_volcano_df_qVals(triqlerFile, protein_id_fdr_treshold, specie, s1, s2, logFunc = np.log2, side = "two", fc_treshold = 1.0, fc_dev = 0.2, p_treshold = 0.05, col_diffExp = "red", col_not_diffExp = "blue", pickle_path = "../data/pickled/"):
+    """
+    s1,s2 = S01...S10
+    """
+    # Read data
+    triqler = read_pickle_Triqler(triqlerFile, protein_id_fdr_treshold, pickle_path = pickle_path)
+    at_t, ce_t, hs_t = splitTriqlerBySpecies(triqler, exponential = False, truncated = False)
+    
+    if specie == "HUMAN":
+        df = hs_t
+    elif specie == "ARATH":
+        df = at_t
+    elif specie == "CAEEL":
+        df = ce_t
+    #define samples
+    sample1 = df[s1]
+    sample2 = df[s2]
+    log2fc_vals = get_point_estimate_log2fc(sample1, sample2)
+    logQ = compute_logQ(sample1, sample2, logFunc)
+    log2fc_vals = log2fc_vals.reindex(index = logQ.index)
+    df_volcano_format = volcano_df_format(log2fc_vals, logQ, logFunc = logFunc, side = side, fc_treshold = fc_treshold, fc_dev = fc_dev, p_treshold = p_treshold, col_diffExp = col_diffExp, col_not_diffExp = col_not_diffExp)
+    return df_volcano_format
+
+
+###############
+# Spectronaut #
+###############
+
+def get_spectronaut_volcano_df_pVals(spectronautFile, protein_id_fdr_treshold, specie, s1, s2, logFunc = np.log2, side = "two", fc_treshold = 1.0, fc_dev = 0.2, p_treshold = 0.05, col_diffExp = "red", col_not_diffExp = "blue", pickle_path = "../data/pickled/"):
+    """
+    s1,s2 = S01...S10
+    """
+    spectronaut =  read_pickle_Spectronaut(spectronautFile, protein_id_fdr_treshold, impute = None, global_impute = True, pickle_path = pickle_path)
+    at_s, ce_s, hs_s = splitSpectronautBySpecies(spectronaut, truncated = False)
+
+    if specie == "HUMAN":
+        df = hs_s
+    elif specie == "ARATH":
+        df = at_s
+    elif specie == "CAEEL":
+        df = ce_s        
+    #define samples
+    sample1 = df[s1]
+    sample2 = df[s2]    
+    log2fc_vals = get_point_estimate_log2fc(sample1, sample2)
+    #log2P = compute_log2P(sample1, sample2)    
+    #log10P = compute_log10P(sample1, sample2)
+    logP = compute_logP(sample1, sample2, logFunc)    
+    #df_volcano_format = volcano_df_format(log2fc_vals, log2P[0], side = side, fc_treshold = fc_treshold, p_treshold = p_treshold, col_diffExp = col_diffExp, col_not_diffExp = col_not_diffExp)
+    df_volcano_format = volcano_df_format(log2fc_vals, logP[0], logFunc = logFunc, side = side, fc_treshold = fc_treshold, fc_dev = fc_dev, p_treshold = p_treshold, col_diffExp = col_diffExp, col_not_diffExp = col_not_diffExp)
+    return df_volcano_format
+
+def get_spectronaut_volcano_df_qVals(spectronautFile, protein_id_fdr_treshold, specie, s1, s2, logFunc = np.log2, side = "two", fc_treshold = 1.0, fc_dev = 0.2, p_treshold = 0.05, col_diffExp = "red", col_not_diffExp = "blue", pickle_path = "../data/pickled/"):
+    """
+    s1,s2 = S01...S10
+    """
+    spectronaut =  read_pickle_Spectronaut(spectronautFile, protein_id_fdr_treshold, impute = None, global_impute = True, pickle_path = pickle_path)
+    at_s, ce_s, hs_s = splitSpectronautBySpecies(spectronaut, truncated = False)
+
+    if specie == "HUMAN":
+        df = hs_s
+    elif specie == "ARATH":
+        df = at_s
+    elif specie == "CAEEL":
+        df = ce_s        
+    #define samples
+    sample1 = df[s1]
+    sample2 = df[s2]    
+    log2fc_vals = get_point_estimate_log2fc(sample1, sample2)
+    logQ = compute_logQ(sample1, sample2, logFunc)
+    log2fc_vals = log2fc_vals.reindex(index = logQ.index)
+    df_volcano_format = volcano_df_format(log2fc_vals, logQ, logFunc = logFunc, side = side, fc_treshold = fc_treshold, fc_dev = fc_dev, p_treshold = p_treshold, col_diffExp = col_diffExp, col_not_diffExp = col_not_diffExp)
+    return df_volcano_format
+
+###############
+# computation #
+###############
+    
+def n_diffExp(df):
+    """
+    df - df from get_triqler_volcano_df_pVals() or get_spectronaut_volcano_df_pVals or qVals equivalent.
+    """
+    n = len(df[df.diffExp == True])
+    return n
+
+
+
+
+if __name__ == "__main__":
+    # plot params
+    plotWidth = 1000
+    plotHeight = 1000
+    x_label = "log2(FC)"
+    y_label = "-log10(q-value)"
+    
+    # params    
+    triqlerFile = "tmp"
+    spectronautFile = "tmp"
+    protein_id_fdr_treshold = 0.01
+    fc_treshold = -4.00
+    fc_dev = fc_treshold/2
+    p_treshold = 0.05
+    side = "fc"
+    specie = "CAEEL"
+    s1 = "S02"
+    s2 = "S06"
+    logFunc = np.log10 #Adjust y-label to match this!
+    logP_treshold = -logFunc(p_treshold)
+    vertical_line_r = fc_treshold + fc_dev
+    vertical_line_l = fc_treshold - fc_dev
+
+    triqler_diffExp_col = "firebrick"
+    triqler_not_diffExp_col = "gold" 
+    spectronaut_diffExp_col = "green"
+    spectronaut_not_diffExp_col = "steelblue"
+    
+    triqler_diffExp_col = "steelblue"
+    triqler_not_diffExp_col = "steelblue" 
+    spectronaut_diffExp_col = "green"
+    spectronaut_not_diffExp_col = "green"
+    
+    title_triqler = "Triqler| " + "proteid_id_fdr: " + str(protein_id_fdr_treshold) + "| fc_treshold: " + str(fc_treshold) + "| p_treshold " + str(p_treshold) + "| specie: " + specie + " " + s1 + ":" + s2
+    title_spec = "Spectronaut| " + "proteid_id_fdr: " + str(protein_id_fdr_treshold) + "| fc_treshold: " + str(fc_treshold) + "| p_treshold " + str(p_treshold) + "| specie: " + specie + " " + s1 + ":" + s2
+    title_overlay = "Overlay| " + "proteid_id_fdr: " + str(protein_id_fdr_treshold) + "| fc_treshold: " + str(fc_treshold) + "| p_treshold " + str(p_treshold) + "| specie: " + specie + " " + s1 + ":" + s2
+    
+    outputFile_triqler = "volcano_triqler_"+specie+"_"+s1+"_"+s2 + ".html"
+    outputFile_spec = "volcano_spectronaut_"+specie+"_"+s1+"_"+s2+ ".html"
+    outputFile_overlay = "volcano_overlay_"+specie+"_"+s1+"_"+s2+ ".html"
+    
+    samples = ["S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10"]
+    
+    # ToDo
+    
+    # Make a comparison matrix between samples
+    # get fc between samples 
+    # Find all fcs...
+    # Find all fc_ratios...
+    
+    
+    get_fc_between_samples(s1,s2)
+
+    df_triqler = get_triqler_volcano_df_qVals(triqlerFile, protein_id_fdr_treshold, specie, s1, s2, 
+                                              logFunc = logFunc, side = side, fc_treshold = fc_treshold, fc_dev = fc_dev,
+                                              p_treshold = p_treshold, col_diffExp = triqler_diffExp_col,
+                                              col_not_diffExp = triqler_not_diffExp_col)
+
+    df_spec = get_spectronaut_volcano_df_qVals(spectronautFile, protein_id_fdr_treshold, specie, s1, s2,
+                                               logFunc = logFunc, side = side, fc_treshold = fc_treshold, fc_dev = fc_dev,
+                                               p_treshold = p_treshold, col_diffExp = spectronaut_diffExp_col,
+                                               col_not_diffExp = spectronaut_not_diffExp_col)
+    
+
+    n_diffExp(df_triqler)
+    n_diffExp(df_spec)
